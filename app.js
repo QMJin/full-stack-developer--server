13a5db41ee471d07f69e4d97da7c887c6ee06b0d
qmjin <jin.qimi@northeastern.edu>
2022-11-29 08:26:12 -0800

    assignment 9

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  autoIndex: false,
+  maxPoolSize: 10,
+  socketTimeoutMS: 45000,
+  family: 4
+}
+
 /*mongoose.connect('mongodb://localhost:27017/tuiter')*/
 /*const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);*/
 
-mongoose.connect( 'mongodb://127.0.0.1:27017/tuiter'
-  ||'mongodb+srv://szjqm31:Myjsy@cluster0.s4bkpye.mongodb.net/?retryWrites=true&w=majority', {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-})
+mongoose.connect( 'mongodb://localhost:27017/tuiter'
+  ||'mongodb+srv://szjqm31:Myjsy@cluster0.s4bkpye.mongodb.net/?retryWrites=true&w=majority', options)
 .then(db => console.log('DB is connected'))
 .catch(err => console.log(err));
 
