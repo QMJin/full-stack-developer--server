4a1c0f096495f5e0f6c0b6ecba7ce7553903c3f6
qmjin <jin.qimi@northeastern.edu>
2022-11-28 20:54:41 -0800

    assignment 9

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect( 'mongodb://localhost:27017/tuiter'
   ||'mongodb+srv://szjqm31:Myjsy@cluster0.s4bkpye.mongodb.net/?retryWrites=true&w=majority', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
+  family: 4
 })
 
 const app = express();
